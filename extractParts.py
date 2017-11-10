71dc109ee5f5a6bf81342fee8a4130d5419736d1
Martin Pavlas <you@example.com>
2017-11-10 17:40:21 +0100

    Add functionality to determine plane angle of a block

--- extractParts.py
+++ extractParts.py
@@ -1,4 +1,13 @@
+import Rhino
 import rhinoscriptsyntax as rs
+from math import atan2, pi, asin
+
+def DecomposeXformRotation(arrXform):
+    arrRotate = Rhino.Geometry.Point3d.Unset
+    arrRotate.X = atan2(- arrXform.M21, arrXform.M22)
+    arrRotate.Y = asin(arrXform.M20)
+    arrRotate.Z = atan2(arrXform.M10, arrXform.M00)
+    return arrRotate
 
 
 #pick an object
@@ -8,5 +17,11 @@ objects = rs.GetObjects("Select Curves", rs.filter.instance)
 for id in objects:
     name = rs.ObjectName(id)
     print "Object:", id, name
-    new_id = rs.RotateObject(id, [0, 0, 0], 90, [1, 0, 0], 1);
-    rs.ObjectName(new_id, "copy " + name)
+    r = DecomposeXformRotation(rs.BlockInstanceXform(id))
+    v = round( r[0] * -180/pi, 0) + 0
+    if v < 0: v += 360
+
+    print "v = %.2f" % v
+
+#    new_id = rs.RotateObject(id, [0, 0, 0], 90, [1, 0, 0], 1);
+#    rs.ObjectName(new_id, "copy " + name)
