66fbc5643067d481bc5881fc3d4479ab69c2db47
Martin Pavlas <you@example.com>
2017-11-12 20:54:57 +0100

    Transform to an object-oriented design

--- insertBlockToTop.py
+++ insertBlockToTop.py
@@ -8,10 +8,62 @@
 import Rhino
 import rhinoscriptsyntax as rs
 
+class Stock(object):
+    """An stock of material that will be used for milling components"""
+
+    Xmax  = 0;
+
+    def __init__(self, layer):
+        super (Stock, self).__init__()
+        self.layer = layer
+
+
+
+class Part(object):
+    """A part to be placed on the stock for nesting and cutting purposes"""
+
+
+
+    def __init__(self, id, cplane):
+        super(Part, self).__init__()
+        self.id = id
+        self.size_x = 0
+        self.size_y = 0
+        self.x = 0
+        self.y = 0
+
+        # rotate and copy to the Top plane
+        initial_plane = rs.ViewCPlane(cplane)
+        final_plane = rs.ViewCPlane("Top")
+        xform = rs.XformRotation1(initial_plane, final_plane)
+        part_id = rs.TransformObjects(id, xform, True)
+        rs.SetUserText(part_id, "cplane", "Top")
+
+        # move block to the "stock" layer
+        rs.ObjectLayer(part_id, "stock")
+
+        bbox = rs.BoundingBox(part_id)
+        rs.MoveObject(part_id, Rhino.Geometry.Point3d(0, 0, 0) - bbox[0])
+
+        # place a label with a block name on the the milling part
+        place_label(part_id)
+
+        bbox = rs.BoundingBox(part_id)
+        self.size_x = bbox[1][0] - bbox[0][0]
+        self.size_y = bbox[2][1] - bbox[0][1]
+        self.x = bbox[0][0]
+        self.y = bbox[0][1]
+
+        print "size_x = ", self.size_x, " size_y = ", self.size_y
+        print "x = ", self.x, " y = ", self.y
+
+
+
 #
 # place_label (object)
 #
-# adds a label with a block name to the center of the block on the Front cplane
+# adds a label with a component name to the center of the block
+# on the Front cplane
 #
 def place_label (id):
 
@@ -22,9 +74,10 @@ def place_label (id):
     # calculate center of the parts bounding box
     x = (bbox[1][0] - bbox[0][0]) / 2
     y = (bbox[2][1] - bbox[0][1]) / 2
-    print "XXX>", x, y
-    text_id = rs.AddTextDot(object_name, (Xmax + x + 10, y, 0))
+
+    text_id = rs.AddTextDot(object_name, (x, y, 0))
     rs.ObjectName(text_id, object_name)
+    rs.ObjectLayer(text_id, "labels")
 
     # make a group of the block and the label
     group_id = rs.AddGroup()
@@ -32,16 +85,23 @@ def place_label (id):
 
     return
 
+def determine_cplane (id):
+
+    cplane = rs.GetUserText(id, "cplane")
+    if not cplane:
+        cplane = "Front"
+        rs.SetUserText(id, "cplane", cplane)
+
+    return
 
 
+# define a stock to place parts on
+panel = Stock("stock")
 
-# set active layer to "stock", ie. layer where the nesting will take place
-rs.CurrentLayer("labels")
-
-#pick an object
+#pick an objects
 objects = rs.GetObjects("Select components", rs.filter.instance, True, True)
 
-Xmax  = 0;
+rs.EnableRedraw(False)
 
 #Loop between my objects
 for id in objects:
@@ -52,36 +112,20 @@ for id in objects:
     # in future possibly update such block
 
     # determine plane where the block is constructed
-    cplane = rs.GetUserText(id, "cplane")
-    if not cplane:
-        cplane = "Front"
-        rs.SetUserText(id, "cplane", cplane)
+    cplane = determine_cplane(id)
 
     # move block to the top plane and update its plane property
-    print "plane:", cplane
-    initial_plane = rs.ViewCPlane(cplane)
-    final_plane = rs.ViewCPlane("Top")
-    xform = rs.XformRotation1(initial_plane, final_plane)
-    NewId = rs.TransformObjects(id, xform, True)
-    rs.SetUserText(NewId, "cplane", "Top")
-    print "remapped"
+    part = Part(id, cplane)
 
-    # move of block so that the insert point is at max X of previous
-    # object plus 10 units
-    box = rs.BoundingBox(NewId)
-    rs.MoveObject(NewId, Rhino.Geometry.Point3d(Xmax + 10, 0, 0) - box[0])
-
-    # move block to the "stock" layer
-    rs.ObjectLayer(NewId, "stock")
-
-    # place a label with a block name on the the milling part
-    place_label(NewId)
+    # placement should happen here
 
     # find out the maximum X of the newly created and moved block
-    box = rs.BoundingBox(NewId)
+    box = rs.BoundingBox(part.id)
     if box:
         for i, point in enumerate(box):
-            if Xmax < point[0]:
-                Xmax = point[0]
+            if panel.Xmax < point[0]:
+                panel.Xmax = point[0]
 
-    print "Xmax = ", Xmax
+    print "Xmax = ", panel.Xmax
+
+rs.EnableRedraw(True)
